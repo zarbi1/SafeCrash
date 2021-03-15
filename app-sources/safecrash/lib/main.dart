6e7132facc57f54685eb18be005c02d8abbb11fb
Mattéo BUFFEY <mbuffey@peda.ltpsn.local>
2021-03-15 15:03:14 +0100

    Update main.dart

--- app-sources/safecrash/lib/main.dart
+++ app-sources/safecrash/lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'SafeCrash',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('SafeCrash'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
+  }
+}
+
+
+
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
