33370356aade71b27b901cd5d42faac1f646f1b0
Hadrien Casadesus <hadrien.casa@outlook.fr>
2021-04-28 18:43:51 +0200

    added notifications plugins and vibration permission

---
+++ appsources/SafeCrash/www/js/index.js
@@ -0,0 +1,29 @@
+let consprint = document.getElementById('console');
+function addContacts(){
+    /*
+    navigator.contacts.pickContact( (contact) =>{
+        consprint.innerHTML = JSON.stringify(contact)
+    },
+    () =>{
+        consprint.innerHTML = err
+    }
+    )
+    */
+
+    /*
+    navigator.notification.alert(
+        'You are the winner!',  // message
+        alertDismissed,         // callback
+        'Game Over',            // title
+        'Done'                  // buttonName
+    );
+        */
+
+}
+
+
+document.addEventListener('deviceready', onDeviceReady, true)
+
+
+function onDeviceReady (){
+}
