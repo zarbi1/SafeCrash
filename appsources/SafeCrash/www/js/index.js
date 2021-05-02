01ba6e345087f8e8957cf9ca2aec8a1af54e5db7
Hadrien Casadesus <hadrien.casa@outlook.fr>
2021-05-02 19:59:28 +0200

    adding database

--- appsources/SafeCrash/www/js/index.js
+++ appsources/SafeCrash/www/js/index.js
@@ -1,16 +1,27 @@
 let consprint = document.getElementById('console');
+const PouchDB = require('pouchdb');
+
+let db = new PouchDB('safecrashDB');
+
+
 function addContacts(){
-    /*
     navigator.contacts.pickContact( (contact) =>{
-        consprint.innerHTML = JSON.stringify(contact)
+        let contactSet = [contact.id, contact.name, contact.phoneNumbers]
+        db.put(contactSet, (err, res)=> {
+            if (!err) {
+                console.log('Sucess !')
+            }else{
+                console.error(err);
+            }
+        })
+
     },
     () =>{
-        consprint.innerHTML = err
+        console.log(err)
     }
     )
-    */
-
-    /*
+    
+    /* NOTIFACTION
     navigator.notification.alert(
         'You are the winner!',  // message
         alertDismissed,         // callback
@@ -19,11 +30,15 @@ function addContacts(){
     );
         */
 
+    console.log('Contact Name is: ',db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+        redrawTodosUI(doc.rows);
+      }))
+
 }
 
 
+
 document.addEventListener('deviceready', onDeviceReady, true)
-
-
 function onDeviceReady (){
+    console.log('Device is Ready SafeCrash Starting...')
 }
