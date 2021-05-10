fb92c16335d0cb0f33011857f923c7fa85ba36a4
Hadrien Casadesus <hadrien.casa@outlook.fr>
2021-05-10 22:23:45 +0200

    Added the code for the esp32
    The code for the esp32 is finished. No worries at the end of the preoject I will add a "how to" in the read me to know how to install your SafeCrash device

---
+++ esp sources/BLE_server/BLE_server.ino
@@ -0,0 +1,79 @@
+/*
+    Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleServer.cpp
+    Ported to Arduino ESP32 by Evandro Copercini
+    updates by chegewara
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+#define PINbutton 26
+#define led 14
+
+
+int btnstate;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PINbutton, INPUT);
+  btnstate = digitalRead(PINbutton);
+  pinMode(led, OUTPUT);
+
+  if (btnstate == 0){
+    
+    Serial.println(btnstate);
+    digitalWrite(led, HIGH);//so the user can see that he is in bound mode
+
+    BLEDevice::init("SafeCrash127EBoundMode");
+    BLEServer *pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                           CHARACTERISTIC_UUID,
+                                           BLECharacteristic::PROPERTY_READ |
+                                           BLECharacteristic::PROPERTY_WRITE
+                                         );
+  
+    pCharacteristic->setValue("Hello SafeCrash User");
+    pService->start();
+    // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+  }else{
+    digitalWrite(led, LOW);
+
+    BLEDevice::init("SafeCrash127E");
+    BLEServer *pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                           CHARACTERISTIC_UUID,
+                                           BLECharacteristic::PROPERTY_READ |
+                                           BLECharacteristic::PROPERTY_WRITE
+                                         );
+  
+    pCharacteristic->setValue("Hello SafeCrash User");
+    pService->start();
+    // BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+    pAdvertising->setMinPreferred(0x12);
+    BLEDevice::startAdvertising();
+  }
+  
+}
+
+void loop() {
+  delay(2000);
+}
